bd1558fa839a4db20068645070da329ac7e3a3b5
Jason chester <jason.chester@brandmuscle.com>
2017-11-21 14:34:11 -0500

    package updates & cleanup

--- Source/SpectoLogic.Azure.Graph.Test/AdditionalProperties/IExpandoVertex.cs
+++ Source/SpectoLogic.Azure.Graph.Test/AdditionalProperties/IExpandoVertex.cs
@@ -1,12 +1,4 @@
-﻿using System.Dynamic;
-using System.Linq.Expressions;
-using Dynamitey;
-using Dynamitey.DynamicObjects;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using SpectoLogic.Azure.Graph.Test.Delivery;
-
-namespace SpectoLogic.Azure.Graph.Test.AdditionalProperties
+﻿namespace SpectoLogic.Azure.Graph.Test.AdditionalProperties
 {
     public interface IExpandoVertex : IVertex<IExpandoEdge, IExpandoEdge>
     {
